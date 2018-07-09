c680356bf645d90cabfcdbdd14fc03cbe3c1e42a
pankhi <pankhiwalia@gmail.com>
2018-07-09 14:15:30 +0530

    add files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,51 +9,6 @@ import {Component, NgModule} from '@angular/core';
 
 export class AppComponent{
 	title='Latest Mobile Launch';
-	mobiles=[{
-		name:'samsung',
-		version:'7.1',
-		stock:2,
-		price:11000,
-		availablility:'08/05/2018' //month/date/year
-	},
-	{
-		name:'moto',
-		version:'7.0.0',
-		stock:3,
-		price:15000,
-		availablility:'07/08/2018'
-	},
-	{
-		name:'oneplus',
-		version:'8.0',
-		stock:0,
-		price:30000,
-		availablility:'08/08/2018'
-	},
-	{
-		name:'moto',
-		version:'7.0.0',
-		stock:3,
-		price:15000,
-		availablility:'07/08/2018'
-	},
-	{
-		name:'oneplus',
-		version:'8.0',
-		stock:0,
-		price:30000,
-		availablility:'08/08/2018'
-	}
-	]
 
-
-// creating function and calling this fxn in html file
-	totalStockleft(){
-		let sum=0;
-			for (let mobile of this.mobiles){
-				sum= sum+mobile.stock;	
-			}
-			return sum;
-	}
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {BrowserModule} from '@angular/platform-browser';
+import { MobPartsComponent } from './mob-parts/mob-parts.component';
 
 @NgModule({
 
-	declarations:[AppComponent],
+	declarations:[AppComponent, MobPartsComponent],
 	imports :[BrowserModule],
 	bootstrap: [AppComponent]
 	
